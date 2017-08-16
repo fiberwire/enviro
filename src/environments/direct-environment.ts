55713f4043a918aba828659fff67891e70750dd4
fiberwire <andrew.ramos92@gmail.com>
2017-08-16 09:53:10 -0400

    implemented direct environment
    this is an environment where you set the state directly by sending updates to it

--- src/environments/direct-environment.ts
+++ src/environments/direct-environment.ts
@@ -1,11 +1,29 @@
+import { Subject } from 'rxjs/Subject';
+import { IEnvironment, IEnvironmentOptions, IStateUpdate, ReactiveProperty } from '../index';
 
-import { Subject } from "rxjs/Subject";
-import { IEnvironment, IStateUpdate, ReactiveProperty } from "../index";
+import { Observable, Subscription } from "rxjs/Rx";
 
-export class DirectEnvironment<EState>
-implements
-IEnvironment<EState> {
+export abstract class DirectEnvironment<EState> implements IEnvironment<EState> {
+  public iteration: number = 0;
+
   public state: ReactiveProperty<IStateUpdate<EState>>;
+
   public incomingStates: Subject<IStateUpdate<EState>>;
 
+  public abstract get initialState(): IStateUpdate<EState>;
+
+  private subs: Subscription = new Subscription();
+
+  constructor(public options: IEnvironmentOptions) {
+    this.state.value = this.initialState;
+
+    this.subs
+      .add(this.update(this.incomingStates));
+  }
+
+  public update(stateUpdates: Observable<IStateUpdate<EState>>): Subscription {
+    return stateUpdates.subscribe((s) => {
+      this.state.value = s;
+    });
+  }
 }
