8ff317e25933453fe0d8886a273eaae1963dc9fb
fiberwire <andrew.ramos92@gmail.com>
2017-08-16 00:29:51 -0400

    added index

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import { AgentEnvironment } from "./environments/agent-environment";
+import { DirectEnvironment } from "./environments/direct-environment";
+import { IAgent } from "./interfaces/agent";
+import { IAgentEnvironment } from "./interfaces/agent-environment";
+import { IAgentUpdate } from "./interfaces/agent-update";
+import { IEnvironment } from "./interfaces/environment";
+import { IHistorical } from "./interfaces/historical";
+import { IInteraction } from "./interfaces/interaction";
+import { IStateUpdate } from "./interfaces/state-update";
+import { IEnvironmentOptions } from "./options/environment-options";
+import { IHistoricalOptions } from "./options/historical-options";
+import { ReactiveCollection } from "./reactive-collection";
+import { ReactiveProperty } from "./reactive-property";
+
+export {
+  ReactiveProperty,
+  ReactiveCollection
+}
+
+export {
+  AgentEnvironment,
+  DirectEnvironment
+}
+
+export {
+  IEnvironment,
+  IAgentEnvironment,
+  IHistorical,
+  IStateUpdate,
+  IAgentUpdate,
+  IInteraction
+}
+
+export {
+  IAgent
+}
+
+export {
+  IEnvironmentOptions,
+  IHistoricalOptions
+}
