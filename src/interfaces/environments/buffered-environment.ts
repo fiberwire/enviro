ba0a4f57ac0d3f7d7c2f680e09ae583b617034d6
fiberwire <andrew.ramos92@gmail.com>
2017-08-16 17:36:20 -0400

    added buffered environment interface

---
+++ src/interfaces/environments/buffered-environment.ts
@@ -0,0 +1,8 @@
+import { Observable, Subscription } from 'rxjs/Rx';
+
+import { IEnvironment, IStateUpdate } from "../../index";
+
+export interface IBufferedEnvironment<EState> extends IEnvironment<EState> {
+    bufferUpdates(updates: Observable<IStateUpdate<EState>>): Observable<Array<IStateUpdate<EState>>>;
+    applyUpdates(buffers: Observable<Array<IStateUpdate<EState>>>): Subscription
+}
