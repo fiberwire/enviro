303468f06b792252c4268232d9e59c1517dd6fe0
fiberwire <andrew.ramos92@gmail.com>
2017-08-16 09:48:33 -0400

    added iteration and update() to environment interface

--- src/interfaces/environment.ts
+++ src/interfaces/environment.ts
@@ -1,9 +1,12 @@
-import { Subject } from "rxjs/Subject";
-import { IEnvironmentOptions, IStateUpdate, ReactiveProperty } from "../index";
+import { Observable, Subject, Subscription } from "rxjs/Rx";
+import { IEnvironmentOptions, IStateUpdate, ReactiveProperty } from '../index';
 
 export interface IEnvironment<EState> {
   state: ReactiveProperty<IStateUpdate<EState>>;
   incomingStates: Subject<IStateUpdate<EState>>;
   options: IEnvironmentOptions;
   initialState: IStateUpdate<EState>;
+  iteration: number;
+
+  update(stateUpdates: Observable<IStateUpdate<EState>>): Subscription;
 }
