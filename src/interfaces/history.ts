53f8ac7dcbdae1c32095b56a186ef0d6a0b4fa62
fiberwire <andrew.ramos92@gmail.com>
2017-08-16 15:36:29 -0400

    added history interface

---
+++ src/interfaces/history.ts
@@ -0,0 +1,15 @@
+import { ReactiveCollection } from '../index';
+
+export interface IHistory<T> {
+  history: ReactiveCollection<T>;
+  length: number;
+  maxLength: number;
+
+  newest: T;
+  oldest: T;
+
+  filter(f: (value: T) => boolean): T[];
+  handleOldRecords(value: T): T;
+  record(value: T): T;
+  remove(value: T): T;
+}
