8cfae0683664790cee3f8d9bd2f69195dcc49481
fiberwire <andrew.ramos92@gmail.com>
2017-09-03 10:49:57 -0400

    added more formatting to example output

--- src/examples/agents/agents.ts
+++ src/examples/agents/agents.ts
@@ -4,25 +4,29 @@ import { RandomEnv } from './random-env';
 import * as _ from 'lodash';
 
 const env = new RandomEnv({
-  interactionTime: 333,
+  interactionTime: 1 / 600,
+  interactionsPerAgent: 100,
+  skipInteractions: 1,
 });
 
 console.log(`created environment`);
 
 env.states.subscribe(update => {
   console.log(
-    `[${update.index}]: sum: ${update.state.sum}, mean: ${update.state.mean}`
+    `[${update.index}]: sum: ${update.state.sum} = ${update.state.previousSum} + ${update.state.x}, mean: ${update.state.mean}`
   );
 });
 
 console.log(`subscribed to updates`);
 
 // create agents
-const agents = _.range(1, 6).map(i => new RandomAgent((i * 20) - 19, i * 20));
+const agents = _.range(50).map(
+  i => new RandomAgent((i + 1) * 20 - 19, (i + 1) * 20)
+);
 
 console.log(`created agents`);
 
 // make agents interact with the environment
-agents.forEach(a => a.interactWithEnvironment(env, 100));
+agents.forEach(a => a.interactWithEnvironment(env));
 
 console.log(`made agents interact with environment`);
